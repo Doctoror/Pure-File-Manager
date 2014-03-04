f5417fc85654458616536020f7acf84d28fe97c3
Jaroslav Mytkalyk <jaroslav.mytkalyk@lemberg.co.uk>
2014-03-04 12:20:13 +0200

    Improved file deletion from MediaStore

--- purefm/src/main/java/com/docd/purefm/utils/MediaStoreUtils.java
+++ purefm/src/main/java/com/docd/purefm/utils/MediaStoreUtils.java
@@ -18,11 +18,8 @@ import java.io.File;
 import java.util.List;
 
 import android.content.ContentResolver;
-import android.content.ContentUris;
-import android.database.Cursor;
 import android.net.Uri;
 import android.provider.MediaStore;
-import android.util.Pair;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -30,51 +27,37 @@ public final class MediaStoreUtils {
 
     private MediaStoreUtils() {}
 
+    private static final Uri INTERNAL_CONTENT_URI = MediaStore.Files.getContentUri("internal");
     private static final Uri EXTERNAL_CONTENT_URI = MediaStore.Files.getContentUri("external");
-    
-    private static final String[] FIELDS = {
-            MediaStore.Files.FileColumns._ID
-    };
 
     public static void deleteFile(final ContentResolver contentResolver, final File file) {
-        final Cursor c = contentResolver.query(
-                EXTERNAL_CONTENT_URI,
-                FIELDS,
-                MediaStore.Files.FileColumns.DATA + "=?",
-                new String[]{file.getAbsolutePath()},
-                null);
-        if (c != null) {
-            final long id = c.getLong(c.getColumnIndex(MediaStore.Files.FileColumns._ID));
-            contentResolver.delete(ContentUris.withAppendedId(EXTERNAL_CONTENT_URI, id), null, null);
-            c.close();
-        }
+        final String selection = MediaStore.Files.FileColumns.DATA + "=?";
+        final String[] selectionArgs = new String[] {'\'' + file.getAbsolutePath() + '\''};
+        contentResolver.delete(INTERNAL_CONTENT_URI, selection, selectionArgs);
+        contentResolver.delete(EXTERNAL_CONTENT_URI, selection, selectionArgs);
     }
 
     public static void deleteFiles(final ContentResolver contentResolver, final List<File> files) {
-        final Pair<String, String[]> selectionAndPaths = filesToSelectionAndPaths(files);
-        final Cursor c = contentResolver.query(EXTERNAL_CONTENT_URI, FIELDS, selectionAndPaths.first, selectionAndPaths.second, null);
-        if (c != null) {
-            for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
-                final long id = c.getLong(c.getColumnIndex(MediaStore.Files.FileColumns._ID));
-                contentResolver.delete(ContentUris.withAppendedId(EXTERNAL_CONTENT_URI, id), null, null);
-            }
-            c.close();
-        }
+        final String selection = filesToSelection(files);
+        contentResolver.delete(INTERNAL_CONTENT_URI, selection, null);
+        contentResolver.delete(EXTERNAL_CONTENT_URI, selection, null);
     }
 
     @NotNull
-    private static Pair<String, String[]> filesToSelectionAndPaths(final List<File> files) {
-        final StringBuilder selection = new StringBuilder();
+    private static String filesToSelection(final List<File> files) {
+        final StringBuilder selection = new StringBuilder(MediaStore.Files.FileColumns.DATA);
+        selection.append(" IN ");
+        selection.append('(');
         final int size = files.size();
-        final String[] paths = new String[size];
         for (int i = 0; i < size; i++) {
-            paths[i] = files.get(i).getAbsolutePath();
-            if (selection.length() != 0) {
-                selection.append(" OR ");
+            selection.append('\'');
+            selection.append(files.get(i).getAbsolutePath());
+            selection.append('\'');
+            if (i != size - 1) {
+                selection.append(',');
             }
-            selection.append(MediaStore.Files.FileColumns.DATA).append("=?");
         }
-        return new Pair<String, String[]>(selection.toString(), paths);
+        selection.append(')');
+        return selection.toString();
     }
-    
 }
